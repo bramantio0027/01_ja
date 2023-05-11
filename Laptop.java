2e0ff21ad0fa8a69c61643d8233d6497264ba0a8
bramantio0027 <btiofh24@gmail.com>
2023-05-11 14:56:19 +0700

    Mosimosi

---
+++ Laptop.java
@@ -0,0 +1,20 @@
+//ini contoh class
+public class Laptop{
+    //ini property
+    String pemilik;
+    String merk;
+    Double ukuranlayar;
+
+    //ini method
+    String hidupkanLaptop(){
+        return "HidupkanLaptop";
+    }
+    String matikanLaptop(){
+        return "MatikanLaptop";
+    }
+   
+}
+
+
+
+
